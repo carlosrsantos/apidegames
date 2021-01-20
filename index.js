e081da5a38f3b9db8a8e63388cac03d7aea39ca5
carlosrsantos <66956614+carlosrsantos@users.noreply.github.com>
2021-01-20 11:35:41 -0300

    Finalizando API rest

--- index.js
+++ index.js
@@ -9,40 +9,111 @@ var DB = {
     games: [
         { 
             id: 1,
-            name: "Call of duty",
+            title: "Call of duty",
             year: 2018,
             price: 90
         },
         { 
             id: 23,
-            name: "Mortal Kombat",
+            title: "Mortal Kombat",
             year: 2019,
             price: 112
         },
         { 
             id: 36,
-            name: "GTA San Andreas",
+            title: "GTA San Andreas",
             year: 2017,
             price: 65
         }
     ]
 }
 
+//listar todos games
 app.get('/games',(req, res) => {
     res.statusCode = 200;
     res.json(DB.games);
 });
 
+//listar game por id
 app.get('/game/:id',(req, res) => {
    
     if(isNaN(req.params.id)){
+
         res.sendStatus(400);
+    
     }else{
 
         var id = parseInt(req.params.id);
-        var game = DB.games.find( g => g.id === id);
+        var game = DB.games.find( g => g.id == id);
+        
         if(game != undefined){
+            res.statusCode = 200;
             res.json(game);
+        }else{
+            res.sendStatus(404);
+        }
+    }
+});
+
+//cadastrar game
+app.post("/game", (req, res)=>{
+    
+    var { title, year, price } = req.body;
+
+    DB.games.push({
+        id: 58,
+        title,
+        year,
+        price
+    });
+
+    res.sendStatus(200);
+
+});
+
+//deletar game
+app.delete("/game/:id",(req, res)=> {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        var id = parseInt(req.params.id);
+        var index = DB.games.findIndex(g => g.id == id);
+
+         if(index == -1){
+            res.sendStatus(404);
+        }else{
+            DB.games.splice(index, 1);
+            res.sendStatus(200);
+        }
+    }
+});
+
+//edição de games
+app.put("/game/:id", (req, res)=>{
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        var id = parseInt(req.params.id);
+        var game = DB.games.find( g => g.id == id);
+        
+        if(game != undefined){
+            
+            var {title, year, price} = req.body;
+
+            if(title != undefined){
+                game.title = title;
+            }
+
+            if(year != undefined){
+                game.year = year;
+            }
+
+            if(price != undefined){
+                game.price = price;
+            }
+
+            res.sendStatus(200);
+
         }else{
             res.sendStatus(404);
         }

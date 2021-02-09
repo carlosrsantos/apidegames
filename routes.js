a03d86ec8f4f02120f54547a36f1eafd814ccb1a
carlosrsantos <66956614+carlosrsantos@users.noreply.github.com>
2021-02-09 16:55:48 -0300

    Add jwt

--- routes.js
+++ routes.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const User = require('./models/User');
 const Game = require('./models/Game');
+const jwt = require('jsonwebtoken');
 
 const route = express();
 
+//palavra chave
+const secret = "$Unavaibled@System.000";
+
 //listar games
-route.get('/games',(req, res) => {
+route.get('/games',auth,(req, res) => {
     res.statusCode = 200;
-    //res.json(DB.games);
     Game.findAll().then( games => {
         res.json(games);
     });
 });
+
+function auth(req, res, next){
+    const authToken = req.headers['authorization']; 
+    if(authToken){
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+        jwt.verify(token, secret, (err, data)=>{
+            if(err){
+                res.status(401);
+            }else{
+                req.token = token;
+                req.loggedUser = { id: data.id, email: data.email };
+                next();
+            }
+        });
+
+
+    }else{
+        res.status(401);
+    }
+    
+}
 
 //listar game por id
 route.get('/game/:id',(req, res) => {
@@ -23,7 +48,7 @@ route.get('/game/:id',(req, res) => {
     }else{
 
         var id = parseInt(req.params.id);
-       // var game = Game.find( g => g.id == id);
+       
         Game.findByPk(id).then(game => {        
                 if(game != undefined){
                     res.statusCode = 200;   
@@ -89,7 +114,7 @@ route.put("/game/:id", (req, res)=>{
         res.sendStatus(400);
     }else{
         var id = parseInt(req.params.id);
-        //var game = DB.games.find( g => g.id == id);
+        
         Game.findByPk(id).then(game => {
             if(game != undefined){
             
@@ -126,8 +151,18 @@ route.post('/auth',(req, res)=>{
         User.findOne({ where: { email }})
         .then(user => {        
                 if(user.password == password){
-                    res.status(200);   
-                    res.json({token: "TOKEN inválido!"});
+
+                    //payload ... expiresIn não utilize valor alto
+                    jwt.sign({ id: user.id, email: user.email },secret,{expiresIn:'48h'},(err, token)=>{
+                        if(err){
+                            res.status(400);
+                            res.json({err:"Falha interna!"});
+                        }else{
+                            res.status(200);   
+                            res.json({token: token});
+                        }
+                    });
+
                 }else{
                     res.status(401);
                     res.json({err:"Credenciais inválidas!"});
